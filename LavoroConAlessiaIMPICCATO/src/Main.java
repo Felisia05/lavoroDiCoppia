30fa1a9c1a10df40a8c7006efd90f8f7d37538a8
Alessia Ercolino <alessiaercolino@Aless-MacBook.local>
2026-09-18 12:10:45 +0200

    creazione struttura

--- LavoroConAlessiaIMPICCATO/src/Main.java
+++ LavoroConAlessiaIMPICCATO/src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int nuova=5;
-        int somma=nuova+10;
-        int differenza=nuova-3;
-        int prodotto=nuova*2;
-        
-        System.out.println("La somma è: " + somma);
+
+    public static void sceltaIncognita(){
+
+    }
+
+    public static void inserimento_lettera(){
+        //passaggio della parola dalla funzione di sopra
+        sceltaIncognita();
 
 
     }
+
+    public static void main(String[] args) {
+        inserimento_lettera();
+
+    }
+
 
 }
